ab52c5fd55a1ee0c2b76e79e12cf8c0d5ffea1f5
Yvan Portelance <95664803+yportelance@users.noreply.github.com>
2022-07-18 10:50:09 -0400

    Update streamlit_app.py
    Updated title + 1st header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Mom''s New Healthy Diner')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
