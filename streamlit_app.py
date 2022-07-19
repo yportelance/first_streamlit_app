8dbcd61e6ba141a17c04ba79b09139df52020628
Yvan Portelance <95664803+yportelance@users.noreply.github.com>
2022-07-19 00:21:51 -0400

    Update streamlit_app.py
    change for kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-# write your own comment -what does the next line do? 
+# Normalize semi-structured JSON data into a flat table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# Affiche le tableau
 streamlit.dataframe(fruityvice_normalized)
