d1204782f00b4c9b7666d740a92a8bbf576fb97e
Yvan Portelance <95664803+yportelance@users.noreply.github.com>
2022-07-19 23:19:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,3 +53,4 @@ def get_fruit_load_list():
 # add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 # streamlit.write('Thanks for adding ', add_my_fruit)
 # my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
