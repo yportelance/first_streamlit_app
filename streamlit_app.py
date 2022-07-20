bb9d0f6af7b3ac21b04a235ad200f5785a8df0b7
Yvan Portelance <95664803+yportelance@users.noreply.github.com>
2022-07-19 23:16:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,6 @@ try:
     streamlit.dataframe(back_from_function)
 except RLError as e:
   streamlit.error()
-    
-streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
